fd35e517ac2dfaa2744b95b03ca9b0f341df21b5
FoundIzalith <evapowlison@protonmail.com>
2024-12-05 17:21:27 -0500

    Graph dbscan

--- clusterAnalysis.py
+++ clusterAnalysis.py
@@ -10,7 +10,7 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
-
+import numpy as np
 
 CONSOLE_COLOR_RED = "\033[91m"
 CONSOLE_COLOR_MAG = "\033[95m"
@@ -79,7 +79,49 @@ def reduceDimensionality(data, n_components=2):
 def dbscanClustering(data):
     dbscan = DBSCAN(eps=0.1, min_samples=10, n_jobs=-1, algorithm='ball_tree', leaf_size=15)
     labels = dbscan.fit_predict(data)
-    print(f"DBSCAN produced {len(set(labels)) - (1 if -1 in labels else 0)} clusters")
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+
+    print(f"DBSCAN produced {n_clusters} clusters")
+
+    plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis')
+    plt.title("DBSCAN Cluster Results")
+
+    unique_labels = set(labels)
+    core_samples_mask = np.zeros_like(labels, dtype=bool)
+    core_samples_mask[dbscan.core_sample_indices_] = True
+
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = labels == k
+
+        xy = data[class_member_mask & core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=14,
+        )
+
+        xy = data[class_member_mask & ~core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=6,
+        )
+
+    plt.title(f"Estimated number of clusters: {n_clusters}")
+
+    plt.savefig("dbscanResults.png")
+    plt.show()
 
     return labels
 
